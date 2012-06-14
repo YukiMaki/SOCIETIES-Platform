66e6ebe524e5c7118c4a19f63c7043b5945d6781
Mitja Vardjan <mitja.vardjan@setcce.si>
2012-06-14 16:54:04 +0200

    added URL parameter

--- domain-authority/webapp-rest/src/main/java/org/societies/domainauthority/webapp/rest/MyResource.java
+++ domain-authority/webapp-rest/src/main/java/org/societies/domainauthority/webapp/rest/MyResource.java
@@ -2,22 +2,39 @@ package org.societies.domainauthority.webapp.rest;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /** Example resource class hosted at the URI path "/myresource"
  */
 @Path("/myresource")
 public class MyResource {
     
-    /** Method processing HTTP GET requests, producing "text/plain" MIME media
-     * type.
+	private static Logger LOG = LoggerFactory.getLogger(MyResource.class);
+
+	/**
+	 * URL parameter
+	 */
+	public static final String KEY = "key";
+	
+	/**
+     * Method processing HTTP GET requests, producing "application/java-archive" MIME media type.
+     * 
      * @return String that will be send back as a response of type "text/plain".
      */
+	@Path("{name}.jar")
     @GET 
-    @Produces("text/plain")
-    public byte[] getIt() {
+    @Produces("application/java-archive")
+    public byte[] getIt(@PathParam("name") String name, @QueryParam(KEY) String key) {
+
+		LOG.debug("HTTP GET: name = {}, key = {}", name, key);
+		
 		byte[] result = new byte[] {'a', 'h', 'o', 'j'};
-		//return "Hi there!";
+
 		return result;
     }
 }
