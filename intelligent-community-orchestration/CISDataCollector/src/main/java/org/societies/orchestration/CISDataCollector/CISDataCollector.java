8057732a020655d701225be009978c33ee9b5240
Bjørn Magnus Mathisen <bjornmagnus.mathisen@sintef.no>
2012-08-07 09:09:08 +0200

    starting merging datacollector

--- intelligent-community-orchestration/CISDataCollector/src/main/java/org/societies/orchestration/CISDataCollector/CISDataCollector.java
+++ intelligent-community-orchestration/CISDataCollector/src/main/java/org/societies/orchestration/CISDataCollector/CISDataCollector.java
@@ -25,7 +25,7 @@
  * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-package main.java.org.societies.orchestration.CISDataCollector;
+package org.societies.orchestration.CISDataCollector;
 
 import org.societies.api.context.CtxException;
 import org.societies.api.context.event.CtxChangeEvent;
